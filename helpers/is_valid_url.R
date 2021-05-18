3a3b04538babefd32d84205cb006bd1eb94bcfed
David Gyurko <mikann56@gmail.com>
2021-05-18 20:57:11 +0200

    forgotten helper function

---
+++ helpers/is_valid_url.R
@@ -0,0 +1,9 @@
+is_valid_url <- function(x) {
+  pattern <- "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)"
+  stringr::str_detect(x, pattern = pattern)
+}
+
+get_url <- function(x) {
+  pattern <- "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)"
+  stringr::str_extract(x, pattern = pattern)
+}
